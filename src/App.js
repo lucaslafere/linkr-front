8d9cdaa049101e00714e97496199fd0f696ee2e9
Lucas Lafèré <laferelucas@gmail.com>
2022-08-09 15:44:10 -0300

    changed namecontext to usercontext

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { TokenContext } from "./Contexts/TokenContext";
-import { NameContext } from "./Contexts/NameContext";
+import { UserContext } from "./Contexts/UserContext";
 
 export default function App() {
   const [token, setToken] = useState("");
-  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
 
   return (
     <TokenContext.Provider value={{ token, setToken }}>
-      <NameContext.Provider value={{ name, setName }}>
+      <UserContext.Provider value={{ avatar, setAvatar }}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginScreen />} />
             <Route path="/sign-up" element={<SignUpScreen />} />
           </Routes>
         </BrowserRouter>
-      </NameContext.Provider>
+      </UserContext.Provider>
     </TokenContext.Provider>
   );
 }
