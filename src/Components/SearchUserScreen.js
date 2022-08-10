f5cbb0466e4be5c1eccb12da5f9dfec2b349609f
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-10 18:09:00 -0300

    render-search-users

--- src/Components/SearchUserScreen.js
+++ src/Components/SearchUserScreen.js
@@ -5,28 +5,54 @@ import TokenContext from "../Contexts/TokenContext";
 import UserContext from "../Contexts/UserContext";
 import { ThreeDots } from "react-loader-spinner";
 import { FaBeer } from 'react-icons/fa';
+import RenderSearchUser from "../Pages/RenderSearchUser";
 
 export default function SerchUserScreen() {
     const [search, setSearch] = useState(""); 
+    //const [searchUsers, setSearchUsers] = useState([]);
+
+    const searchUsers = [ 
+        {
+            image: "https://tntsports.com.br/__export/1650121510074/sites/esporteinterativo/img/2022/04/16/cristiano_ronaldo_vibrando_-_premier_league.jpg_1359985831.jpg",
+            username: "cr7"
+        }, 
+        {
+            image: "https://tntsports.com.br/__export/1650121510074/sites/esporteinterativo/img/2022/04/16/cristiano_ronaldo_vibrando_-_premier_league.jpg_1359985831.jpg",
+            username: "cr7"
+        }
+    ];
 
     return( 
         <>
         <Header>
             <a>linkr</a>
             <InputText>
-            <input
-                type="text"
-                placeholder="Search for people"
-                value={search}
-                onChange={(event) => setSearch(event.target.value)}
-                required
-            />
-            </InputText>
+                <input
+                    type="text"
+                    placeholder="Search for people"
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                    required
+                />
+            </InputText> 
             <LoggedUser>
                 <FaBeer />
                 <img src="https://tntsports.com.br/__export/1650121510074/sites/esporteinterativo/img/2022/04/16/cristiano_ronaldo_vibrando_-_premier_league.jpg_1359985831.jpg" />
             </LoggedUser>
         </Header> 
+
+        <Search>
+            <ul>
+            {searchUsers.map((users,index) => (
+                <RenderSearchUser 
+                    index= {index}
+                    image= {users.image}
+                    username= {users.username}
+                />
+            ))}
+            </ul>
+        </Search>
+
         </>
     )
  }
@@ -60,6 +86,7 @@ export default function SerchUserScreen() {
         width: 95%; 
         height: 100%;
         display: flex;  
+        font-weight: 100;
         color: rgba(198, 198, 198, 1); 
         background-color: white;
         border-radius: 8px; 
@@ -79,5 +106,22 @@ export default function SerchUserScreen() {
         height: 53px;
         border-radius: 50%; 
         margin-left: 17px;
+    }
+ `
+ const Search = styled.div`
+    width: 28.4%;
+    height: 100%; 
+    display: flex; 
+    flex-direction: column;
+    position: relative;
+    top: 58px;
+    left: 665px;
+    z-index: 1;
+    
+    ul { 
+        width: 100%; 
+        height: 100%;
+        background-color: rgba(231, 231, 231, 1); 
+        border-radius: 8px; 
     }
  `
---
+++ src/Pages/RenderSearchUser.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"; 
+
+export default function RenderSearchUser({index,image,username}) { 
+    return( 
+        <UserCard value={index}>
+            <img src={image} />
+            <a>{username}</a>
+        </UserCard> 
+    )
+} 
+
+const UserCard = styled.li`
+    width: 100%;
+    height: 100%;
+    display: flex; 
+    align-items: center;
+    padding: 14px 17px;
+
+    img { 
+        width: 39px;
+        height: 39px; 
+    }
+
+    a { 
+        margin-left: 12px;
+        font-size: 19px; 
+        color: rgba(81, 81, 81, 1);
+    } 
+
+    &:hover { 
+        cursor: pointer;
+    }
+ `
+
