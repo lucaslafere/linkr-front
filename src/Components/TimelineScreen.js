cb8f4cf4b319a69e79eaae523cdb118c79424279
HugoAseka <hugomaseka@gmail.com>
2022-08-15 11:59:49 -0300

    update from main

--- src/Components/TimelineScreen.js
+++ src/Components/TimelineScreen.js
@@ -4,12 +4,13 @@ import axios from "axios";
 import { ReactTagify } from "react-tagify";
 import UserContext from "../Contexts/UserContext.js";
 import { useNavigate } from "react-router-dom";
+import RenderUserPosts from "../Pages/RenderUserPosts.js";
 
 export default function FeedScreen() {
-const {userData, setUserData} = useContext(UserContext);
-const navigate = useNavigate();
+  const { userData, setUserData } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  const token = localStorage.getItem('MY_TOKEN');
+  const token = localStorage.getItem("MY_TOKEN");
   const [posts, setPosts] = useState([]);
   const [description, setDescription] = useState("");
   const [url, setUrl] = useState("");
@@ -17,7 +18,7 @@ const navigate = useNavigate();
   const [feedMessage, setFeedMessage] = useState("Loading");
   const URL = "https://projeto17-linkrback.herokuapp.com/posts";
   //const URL = "http://localhost:4100/posts";
-  console.log(posts);
+
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -25,7 +26,7 @@ const navigate = useNavigate();
   };
 
   useEffect(() => {
-    const promise = axios.get(URL,config);
+    const promise = axios.get(URL, config);
     promise
       .then((res) => {
         setPosts([...res.data]);
@@ -69,20 +70,14 @@ const navigate = useNavigate();
         <h1>linkr</h1>
         <div>
           <ion-icon name="chevron-down-outline"></ion-icon>
-          <img
-            src={userData.profilePhoto}
-            alt="profile"
-          />
+          <img src={userData.profilePhoto} alt="profile" />
         </div>
       </TopBar>
 
       <Feed>
         <h3>timeline</h3>
         <NewPost>
-          <img
-            src={userData.profilePhoto}
-            alt="profile"
-          />
+          <img src={userData.profilePhoto} alt="profile" />
           <Box fontColor={"#9f9f9f"}>
             <p>What are you going to share today?</p>
             <Input
@@ -105,22 +100,24 @@ const navigate = useNavigate();
         {posts.length === 0 ? (
           <span>{feedMessage}</span>
         ) : (
-          posts.map(({ userId,profilePhoto, description, url, username }, index) => {
-            return (
-              <Post key={index}>
-                <img src={profilePhoto} alt="profile" onClick={() => navigate(`/timeline/${userId}`)}/>
-                <Box fontColor={"white"}>
-                  <h3>{username}</h3>
-                  <ReactTagify colors={"#ffffff"} tagClicked={(tag) => alert(tag)}>
-                    <span>{description}</span>
-                  </ReactTagify>
-                  <a target={"_blank"} href={url}>
-                    {url}
-                  </a>
-                </Box>
-              </Post>
-            );
-          })
+          <Post>
+                <ul>
+                    {posts.map(({url,description,username,profilePhoto,urlDescription,urlImage,urlTitle,id},index) => (
+                        <RenderUserPosts 
+                            index={index}
+                            likes= {99}
+                            url={url}
+                            description={description}
+                            username={username}
+                            profilePhoto={profilePhoto}
+                            urlDescription={urlDescription}
+                            urlImage={urlImage}
+                            urlTitle={urlTitle}
+                            id={id}
+                        />
+                    ))}
+                </ul>
+            </Post>
         )}
       </Feed>
     </Container>
@@ -141,10 +138,10 @@ const TopBar = styled.div`
   width: 100vw;
   display: flex;
   justify-content: space-between;
-  position:fixed;
-  left:0;
-  top:0;
-  z-index:1;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 1;
   h1 {
     color: white;
     font-family: "Passion One";
@@ -211,6 +208,9 @@ const NewPost = styled.div`
     padding-left: 10px;
     margin-top: 10px;
   }
+  @media (max-width:1000px) {
+    width: 100vw;
+  }
 `;
 
 const Box = styled.div`
@@ -246,7 +246,7 @@ const Box = styled.div`
     padding: 10px 0;
   }
   span {
-    color:#B7B7B7;
+    color: #b7b7b7;
   }
 `;
 
@@ -305,18 +305,23 @@ const Button = styled.button`
 `;
 // ${({color})};
 const Post = styled.div`
-  display: flex;
-  height: 20vh;
-  background-color: #171717;
   width: 40vw;
-  border-radius: 14px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  img {
-    height: 50px;
-    width: auto;
-    border-radius: 50%;
-    color: #ffffff;
-    padding-left: 10px;
-    margin-top: 10px;
+  ul {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    height: 100%;
+
+    ul {
+      width: 100%;
+    }
   }
 `;
