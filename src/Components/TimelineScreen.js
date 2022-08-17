238e8a81fd907a6e2fbabaae079faaa0b3968db3
HugoAseka <hugomaseka@gmail.com>
2022-08-17 20:06:43 -0300

    feat: infinite scroll

--- src/Components/TimelineScreen.js
+++ src/Components/TimelineScreen.js
@@ -26,12 +26,9 @@ export default function FeedScreen() {
   const [search, setSearch] = useState([]);
   const { token, setToken } = useContext(TokenContext);
   const navigate = useNavigate();
-
   const URL = "http://localhost:4000/posts/";
-
   const data = JSON.parse(localStorage.getItem("userInfo"));
-
-  const backendURL = "https://projeto17-linkrback.herokuapp.com/posts";
+  const backendURL = "https://projeto17-linkrback.herokuapp.com/posts/";
 
   const config = {
     headers: {
@@ -40,10 +37,8 @@ export default function FeedScreen() {
   };
 
   async function getPosts(queryLimit) {
-  
     return axios.get(backendURL + queryLimit, config)
     .then((response) => {
-      console.log(response.data,'response.data')
       if (response.data.length === 0) setFeedMessage("There are no posts yet");
       return(response.data);
     })
@@ -70,8 +65,8 @@ export default function FeedScreen() {
         setLoading(false);
         setDescription("");
         setUrl("");
-        const data = await getPosts(posts.length);
-        setPosts(data);
+        const postsData = await getPosts(posts.length);
+        setPosts(postsData);
       })
       .catch(() => {
         window.alert("Houve um erro ao publicar seu post, tente novamente.");
@@ -144,36 +139,17 @@ export default function FeedScreen() {
     return items;
   };
   const loadMore = async () => {
-    console.log("load");
-    console.log("querylimit", queryLimit);
-    console.log('hasmoreitems',hasMoreItems)
-    // console.log("posts",posts.length)
-
     if (queryLimit > posts.length + 10) {
       setHasMoreItems(false); 
     } else {
-      const data = await getPosts(queryLimit);
-      setPosts(data);
-      console.log(data,'data');
+      const postsData = await getPosts(0);
+      if(!postsData) postsData = [];
+      setPosts(postsData);
       setQueryLimit(queryLimit+itemsPerPage)
       setRecords(records+itemsPerPage)
     }
   
   };
-  // const loadMore = () => {
-  //   let limit = records;
-  //   if (records > posts.length) limit = posts.length;
-  //   if (limit === posts.length) {
-  //     setHasMoreItems(false);
-  //   } else {
-  //     setTimeout(() => {
-  //       setRecords(records + itemsPerPage);
-  //       // posts.length - records > 10
-  //       //   ? setRecords(records + 10)
-  //       //   : setRecords(records + 15);
-  //     }, 100);
-  //   }
-  // };
   
 
   return (
@@ -297,29 +273,6 @@ export default function FeedScreen() {
                 </Button>
               </Box>
             </NewPost>
-            {/* {posts.length === 0 ? (
-              <span>{feedMessage}</span>
-            ) : (
-              posts.map((object, index) => (
-                <PostBox
-                  id={object.id}
-                  key={index}
-                  url={object.url}
-                  profilePhoto={object.profilePhoto}
-                  username={object.username}
-                  description={object.description}
-                  urlDescription={object.urlDescription}
-                  urlTitle={object.urlTitle}
-                  urlImage={object.urlImage}
-                  likes={object.likes}
-                  setIdDeleting={setIdDeleting}
-                  setDeleting={setDeleting}
-                  setUpdatePosts={setUpdatePosts}
-                  updatePosts={updatePosts}
-                  userId={object.userId}
-                />
-              ))
-            )} */}
             <div
               style={{
                 overflow: "auto",
