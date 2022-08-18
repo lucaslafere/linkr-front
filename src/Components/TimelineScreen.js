86fdb2b2992814b48e61efaa8019cb04566c7356
Lucas Lafèré <laferelucas@gmail.com>
2022-08-18 18:51:25 -0300

    fix timeline

--- src/Components/TimelineScreen.js
+++ src/Components/TimelineScreen.js
@@ -271,7 +271,7 @@ export default function FeedScreen() {
                 </Button>
               </Box>
             </NewPost>
-            {posts.length === 0 ? (
+            {/* {posts.length === 0 ? (
               <span>{feedMessage}</span>
             ) : (
               posts.map((object, index) => (
@@ -295,7 +295,7 @@ export default function FeedScreen() {
                   setRepostId={setRepostId}
                 />
               ))
-            )}
+            )} */}
             <InfiniteScroll
               loadMore={loadMore}
               hasMore={hasMoreItems}
